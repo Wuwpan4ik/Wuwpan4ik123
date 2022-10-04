514ffc4e90f85cce260476d878ca7d5894b07250
Wuwpan4ik <dimalim110@gmail.com>
2022-10-05 00:16:51 +0500

    addInputsToVideoEdit

--- controller/ProjectController.php
+++ controller/ProjectController.php
@@ -38,6 +38,7 @@
 
                 }
             }
+            return True;
         }
 
         public function createDir () {
@@ -80,32 +81,33 @@
 
         public function renameVideo() {
             $courseContent = $this->m->db->query("SELECT * FROM `course_content` WHERE id = ".$_GET['id']);
-            print_r($courseContent);
-            $res = $this->m->db->query("SELECT * FROM `course_content` WHERE id = ".$courseContent[0]['course_id']);
-            print_r($res);
-//            if (!$this->isUser($res[0]['author_id'])) return False;
+            $res = $this->m->db->query("SELECT * FROM `course` WHERE id = ".$courseContent[0]['course_id']);
+            if (!$this->isUser($res[0]['author_id'])) return False;
 
             $name = $_POST['name'];
-            $this->m->db->execute("UPDATE `course_content` SET `name`='$name' WHERE id = " . $_GET['id']);
-
+            $description = $_POST['description'];
+            $button_text = $_POST['button_text'];
+            $this->m->db->execute("UPDATE `course_content` SET `name` = '$name', `description` = '$description',
+                            `button_text` = '$button_text' WHERE `id` = " . $_GET['id']);
+            return True;
         }
 
         public function get_content()
         {
-//            echo '<!DOCTYPE html>
-//			<html lang="en">
-//			<head>
-//			<meta charset="UTF-8">
-//			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-//			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-//			<title>Document</title>
-//			</head>
-//			<body>
-//				<script>
-//				    let a = location.protocol + \'//\' + location.host + location.pathname + \'?option=project\';
-//					window.location.replace(a);
-//				</script>
-//			</body>
-//			</html>';
+            echo '<!DOCTYPE html>
+			<html lang="en">
+			<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Document</title>
+			</head>
+			<body>
+				<script>
+				    let a = location.protocol + \'//\' + location.host + location.pathname + \'?option=project\';
+					window.location.replace(a);
+				</script>
+			</body>
+			</html>';
         }
     }
