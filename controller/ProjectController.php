81bdc830fb9883f6b9747dd5bb6f8902f23e86fc
Dima <61015330+Wuwpan4ik@users.noreply.github.com>
2022-10-09 20:56:49 +0500

    Update ProjectController.php

--- controller/ProjectController.php
+++ controller/ProjectController.php
@@ -1,77 +1,28 @@
 <?php
     class ProjectController extends ACore
     {
-        private function isUser($checkId) {
-            return $_SESSION['user']['id'] == $checkId;
-        }
-
-        public function setName()
+        private function isUser($checkId)
         {
-            $res = $this->m->db->query("SELECT * FROM course WHERE id = ".$_GET['id']);
-            if (!$this->isUser($res[0]['author_id'])) return False;
-
-            $idDir = $_GET['id'];
-
-            if($_POST['title'] != "") {
-
-                $name = $_POST['title'];
-
-                rename("./uploads/projects/" . $idDir . "_" . $res[0]['name'], "./uploads/projects/" . $idDir . "_" . $name);
-
-                $pathname = $this->m->db->query("SELECT * FROM course_content WHERE course_id = ".$res[0]['id']);
-
-                $this->m->db->execute("UPDATE `course` SET `name`='$name' WHERE id = " . $res[0]['id']);
-
-                $updater = $this->m->db->link->prepare("UPDATE `course_content` SET `video` = ? WHERE id = ?");
-
-                foreach ($pathname as $path) {
-
-                    $pages = explode("/", $path[4]);
-
-                    $pages[2] = $res['author_id'] . "_" . $name;
-
-                    $changed = implode("/", $pages);
-
-                    $updater->bind_param("ss", $changed, $path[0]);
-
-                    $updater->execute();
-
-                }
-            }
-        }
-
-        public function createDir () {
-            $uid = $_SESSION['user']['id'];
-
-            $this->m->db->execute("INSERT INTO course (`id`, `author_id`, `name`, `price`) VALUES (NULL, '$uid', 'Новый проект', 0)");
-
-            $directory = $this->m->db->query("SELECT * FROM course WHERE author_id = ". $uid . " ORDER BY ID DESC LIMIT 1");
-
-            mkdir("./uploads/projects/".$directory[0]['id']."_Новый проект");
-            return True;
+            return $_SESSION['user']['id'] == $checkId;
         }
 
         public function addVideo () {
             $uid = $_GET['id'];
 
-            $res = $this->m->db->query("SELECT * FROM course WHERE author_id = ".$_SESSION['user']['id']." ORDER BY `id` DESC LIMIT 1");
+            $res = $this->m->db->query("SELECT * FROM course WHERE id = '$uid' ORDER BY `id` DESC LIMIT 1");
 
             move_uploaded_file($_FILES['video_uploader']['tmp_name'], "./uploads/projects/".$uid."_".$res[0]['name']."/".$_FILES['video_uploader']['name']);
 
-            $path = "./uploads/projects/".$uid."_".$res[0]['name']."/".$_FILES['video_uploader']['name'];
+            $path = "./uploads/projects/$uid"."_".$res[0]['name']."/".$_FILES['video_uploader']['name'];
 
             $this->m->db->execute("INSERT INTO course_content (`course_id`, `name`, `description`, `video`) VALUES (".$res[0]['id'].",'Укажите заголовок','Укажите описание', '$path')");
         }
 
-        public function deleteDir () {
-
-            $uid = $_SESSION['user']['id'];
         public function renameVideo() {
             $courseContent = $this->m->db->query("SELECT * FROM `course_content` WHERE id = ".$_GET['id_item']);
             $res = $this->m->db->query("SELECT * FROM `course` WHERE id = ".$courseContent[0]['course_id']);
             if (!$this->isUser($res[0]['author_id'])) return False;
 
-            $project = $this->m->db->query("SELECT * FROM course WHERE id = ". $_GET['id'] . " LIMIT 1");
             $name = $_POST['name'];
             $description = $_POST['description'];
             $button_text = $_POST['button_text'];
@@ -80,7 +31,6 @@
             return True;
         }
 
-            if ($project[0]['author_id'] != $uid) {
         public function initVideoButton() {
             //Форма
             $id_video = $_POST['id_item'];
@@ -90,10 +40,6 @@
             } else {
                 return False;
             }
-
-            $this->m->db->execute("DELETE FROM course WHERE id = ". $_GET['id']);
-            rmdir("./uploads/projects/".$_GET['id']."_" . $project[0]['name']);
-
             if (isset($_POST['form_id-1'])) {
                 $form_input_1 = $_POST['form_id-1'];
             }
@@ -131,7 +77,6 @@
 			</head>
 			<body>
 				<script>
-				    let a = location.protocol + \'//\' + location.host + location.pathname + \'?option=project\';
 				    let ur = document.URL;
                     var url = new URL(ur);
                     var option = url.searchParams.get("option");
