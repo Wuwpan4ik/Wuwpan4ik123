bb90fd225f973506f5a0f038bdbd65196afea25b
CrackpotDude <62250680+CrackpotDude@users.noreply.github.com>
2022-10-16 21:10:13 +0400

    Update account.php

--- pages/account.php
+++ pages/account.php
@@ -4,5 +4,8 @@ class Account extends ACore
     public function get_content() {
         $result = $this->m->getCurrentUser();
         return $result;
+        
+        $content = $this->m->getTariffs();
+        return $content;
     }
 }
