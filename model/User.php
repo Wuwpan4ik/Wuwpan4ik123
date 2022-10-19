236e8c2af033390dbe79a309d4c4b48aa8069605
CrackpotDude <62250680+CrackpotDude@users.noreply.github.com>
2022-10-19 19:35:55 +0400

    Update User.php

--- model/User.php
+++ model/User.php
@@ -11,6 +11,15 @@
             $result = $this->db->query("SELECT * FROM user");
             return $result;
         }
+        
+        public function getClientList () {
+		$current_date = date("Y-m-d", mktime(0, 0, 0, date('m'), date('d'), date('Y')));
+		$last_date = date("Y-m-d", mktime(0, 0, 0, date('m'), date('d') - 2, date('Y')));
+			
+        $res = $this->db->query("SELECT * FROM clients WHERE creator_id = " . $_SESSION['user']['id'] . " AND achivment_date BETWEEN CAST('$last_date' AS DATE) AND CAST('$current_date' AS DATE)");
+		$query = $this->db->query("SELECT * FROM user WHERE id IN (SELECT client_id FROM clients WHERE creator_id = " . $_SESSION['user']['id'].")");
+		return $result = [$res, $query];
+		}
 
         public function getUserProjects() {
             $result = $this->db->query("SELECT * FROM course WHERE author_id = " . $_SESSION['user']['id'] . " GROUP BY id");
