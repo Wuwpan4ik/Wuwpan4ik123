0299c01044eb00f8ac3260bcfd5242f708dde840
Wuwpan4ik <dimalim110@gmail.com>
2022-10-26 17:35:33 +0500

    addHeaderLink

--- template/default/header.php
+++ template/default/header.php
@@ -13,7 +13,7 @@
 
         <button class="ico_button button-bell"><img class="ico" src="img/Bell.svg">  <div id="msg">5</div></button>
 
-        <button id="apps" class="ico_button">Заявки</button>
+        <button id="apps" class="ico_button" onclick="window.location.replace('?option=Analytics')">Заявки</button>
 
     </div>
 
