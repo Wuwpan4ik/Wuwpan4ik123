d17e48b2a101a531ce4862756721cd8db59c9053
ErgardBechert <70965859+ErgardBechert@users.noreply.github.com>
2022-11-07 19:55:02 +0500

    UserMainUpDate

--- js/charts.js
+++ js/charts.js
@@ -1,12 +1,14 @@
-const ctx = document.getElementById('totalProfit').getContext('2d');
+/*totalProfit*/
 
-const myChart = new Chart(ctx, {
+const ctx1 = document.getElementById('totalProfit').getContext('2d');
+
+const ChartTotalProfit = new Chart(ctx1, {
 
     type: 'line',
     data: {
-        labels: ['','пн', 'вт', 'ср', 'чт', ''],
+        labels: ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
         datasets: [{
-            data: [0, 4, 3, 5, 10],
+            data: [0, 4, 3, 5, 10, 2, 5],
             borderColor: [
                 '#4E73F8'
             ],
@@ -21,15 +23,24 @@ const myChart = new Chart(ctx, {
         }]
     },
     options: {
-        pointRadius: [0, 0, 0, 0,5],
+
+        pointRadius: [0, 0, 0, 0, 0, 0, 5],
         scales: {
+            x: {
+                grid:{
+                    color: '#E1E3E6',
+                    borderDash: [8],
+                    borderColor: '#E1E3E6',
+
+                },
+
+            },
             y: {
                 ticks: {
                     display: false
                 },grid: {
                     display: false
                 }
-
             },
         },
         responsive: true,
@@ -45,4 +56,62 @@ const myChart = new Chart(ctx, {
 });
 
 
+/*profitabilityIndicators*/
+const ctx2 = document.getElementById('profitabilityIndicators').getContext('2d');
 
+const ChartProfitabilityIndicators = new Chart(ctx2, {
+
+    type: 'line',
+    data: {
+        labels: ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+        datasets: [{
+            tension: 0.6,
+            lineCap: 'round',
+            borderJoinStyle: "round",
+            data: [0,0, 0, 2000, 10, 0, 0],
+            borderColor: [
+                '#4E73F8'
+            ],
+            borderWidth: 3,
+            pointBackdropPadding: 2,
+            pointWidth: 2,
+            pointBackgroundWidth: 6,
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 3,
+            pointBorderColor: '#4E73F8',
+
+        }]
+    },
+    options: {
+
+        pointRadius: [0, 0, 0, 6, 0, 0, 0],
+        scales: {
+            x: {
+                ticks: {
+                    display: false
+                },
+                grid:{
+                    display: false
+                },
+            },
+            y: {
+                ticks: {
+                    display: false
+                },
+                grid: {
+                    display: false,
+                    border: false
+                }
+            },
+        },
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false
+            },
+        },
+
+    },
+
+
+});
