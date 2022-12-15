46c8c9d54b6af1be78c240afa2d346c1beda2c35
Wuwpan4ik <dimalim110@gmail.com>
2022-12-15 21:10:13 +0500

    add

---
+++ js/customInputs.js
@@ -0,0 +1,37 @@
+window.onload = () =>{
+    let inputs = document.querySelectorAll('.input_focus input');
+    let inputsLabel = document.querySelectorAll('.input_focus label');
+    let inputClear = document.querySelectorAll('.input_focus span');
+    let feed = document.querySelector('.feed');
+
+    for(let i =0; i < inputs.length; i++){
+        if(inputs[i].value.length > 0){
+            inputsLabel[i].classList.add('activeLabel');
+            inputClear[i].classList.add('has_content');
+        }
+        else {
+            inputsLabel[i].classList.remove('activeLabel');
+            inputClear[i].classList.remove('has_content');
+        }
+        feed.addEventListener('click', () => {
+            inputClear[i].classList.remove('has_content');
+        })
+        inputs[i].addEventListener('input', function(){
+
+            if(inputs[i].value.length > 0){
+                inputsLabel[i].classList.add('activeLabel');
+                inputClear[i].classList.add('has_content');
+            }
+            else {
+                inputsLabel[i].classList.remove('activeLabel');
+                inputClear[i].classList.remove('has_content');
+            }
+        });
+
+        inputClear[i].onclick = () =>{
+            inputsLabel[i].classList.remove('activeLabel')
+            inputs[i].value = "";
+            inputClear[i].classList.remove('has_content')
+        }
+    }
+}
