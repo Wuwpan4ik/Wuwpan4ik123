dce8729d3cbd0ef46a1e6076f41db1d5f49c8c53
Wuwpan4ik <dimalim110@gmail.com>
2022-12-27 19:04:41 +0500

    addd

--- template/FunnelEdit.php
+++ template/FunnelEdit.php
@@ -4,7 +4,9 @@
 
 <head>
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Course Creator - Редактирование воронки</title>
 
     <link rel="stylesheet" href="/css/nullCss.css">
