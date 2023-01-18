64908478da021e7899a0ed1e7ab60d2c0e5c78d2
Wuwpan4ik <dimalim110@gmail.com>
2023-01-18 21:56:10 +0500

    Add Contact Html

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -4,12 +4,21 @@ class ContactController extends ACoreCreator {
     private function GetQuestionHTML($name, $email, $time, $question)
     {
         return '<html lang="RU">
-                        <head>
-                            <meta charset="UTF-8">
-                            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        </head>
-                        <body style="padding:0px;margin:0px;max-width: 800px;font-family: Verdana, Geneva, Tahoma, sans-serif;background: #EFEFEF;">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    </head>
+                    <body style="padding:0px;margin:0px;max-width: 800px;font-family: Verdana, Geneva, Tahoma, sans-serif;background: #EFEFEF;">
+                        <style>
+                            th{
+                                font-size: 16px;
+                    font-weight: 400;
+                    color: #666666;
+                                padding:10px 0px;
+                            }
+                        </style>
+                        <div class="envelope-container" style="max-width: 500px; width:100%; margin:0 auto;">
                             <div class="envelope-body" style="background:white;">
                                 <div class="first_row">
                                     <img style="width:100%;" src="https://course-creator.io/envelope-images/envelope-zayavka.jpg" alt="Добро пожаловать в Course Creator!">
@@ -22,53 +31,58 @@ class ContactController extends ACoreCreator {
                                         <!--Здесь выводим само обращение, которое написал юзер-->
                                         Спасибо, что вы зарегистрировались в Сourse Сreator! Ниже важная информация о вашем аккаунте. Пожалуйста, сохраните это письмо, чтобы можно было обратиться к нему позже.
                                     </div>
-                                    <div class="info_account" style="display:-webkit-box;
-                                    display:-ms-flexbox;
-                                    display:flex;-webkit-box-pack: justify;-ms-flex-pack: justify;justify-content: space-between;gap: 20px;margin-top: 20px;-webkit-box-orient: vertical;-webkit-box-direction: normal;-ms-flex-direction: column;flex-direction: column;">
-                                        <div class="whom" style="padding-top: 30px;border-top: 1px dashed rgba(0, 0, 0, 0.2);margin-bottom: 0px;">
-                                            <div class="person" style="margin-bottom:20px;display:flex; justify-content: space-between">
-                                                <span style="font-size:16px;font-weight:400;color: rgba(0, 0, 0, 0.6);">
-                                                    Откуда пришло:
-                                                </span>
-                                                <span style="font-size:16px;font-weight:400;color: rgba(0, 0, 0, 0.6);">
-                                                    '. $name .'
-                                                </span>
-                                            </div>
-                                            <div class="email" style="margin-bottom:20px;display:flex; justify-content: space-between">
-                                                <span style="font-size:16px;font-weight:400;color: rgba(0, 0, 0, 0.6);">
-                                                    От кого пришло:
-                                                </span>
-                                                <span style="font-size:16px;font-weight:400;color: rgba(0, 0, 0, 0.6);">
-                                                    '. $this->email .'
-                                                </span>
-                                            </div>
-                                            <div class="qiestion" style="margin-bottom:20px;display:flex; justify-content: space-between">
-                                                <span style="font-size:16px;font-weight:400;color: rgba(0, 0, 0, 0.6);">
-                                                    Вопрос:
-                                                </span>
-                                                <span style="font-size:16px;font-weight:400;color: rgba(0, 0, 0, 0.6);">
-                                                    '. $question .'
-                                                </span>
-                                            </div>
-                                            <div class="phone" style="display:flex; justify-content: space-between">
-                                                <span style="font-size:16px;font-weight:400;color: rgba(0, 0, 0, 0.6);">
-                                                    Когда пришло:
-                                                </span>
-                                                <span style="font-size:16px;font-weight:400;color: rgba(0, 0, 0, 0.6);">
-                                                    '. date("h:i, d.m.Y") .'
-                                                </span>
-                                            </div>
-                                        </div>
+                                    <div class="info_account" style="margin-top: 20px;">
+                                        <table style="width:100%;padding-top: 30px;border-top: 1px dashed rgba(0, 0, 0, 0.2);margin-bottom: 0px;">
+                                            <thead>
+                                                <tr>
+                                                    <th style="text-align:start;">
+                                                        Откуда пришло:
+                                                    </th>
+                                                    <th></th>
+                                                    <th style="text-align:end;">
+                                                        '. $name .'
+                                                    </th>
+                                                </tr>
+                                                <tr>
+                                                    <th style="text-align:start;">
+                                                        От кого пришло:
+                                                    </th>
+                                                    <th></th>
+                                                    <th style="text-align:end;">
+                                                        '. $email .'
+                                                    </th>
+                                                </tr>
+                                                <tr>
+                                                    <th style="text-align:start;">
+                                                        Текст вопроса:
+                                                    </th>
+                                                    <th></th>
+                                                    <th style="text-align:end;">
+                                                        '. $question .'
+                                                    </th>
+                                                </tr>
+                                                <tr>
+                                                    <th style="text-align:start;">
+                                                        Когда пришло:
+                                                    </th>
+                                                    <th></th>
+                                                    <th style="text-align:end;">
+                                                        '. $time .'
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                        </table>
                                     </div>
                                 </div>
                             </div>
                             <div class="envelope_info_bottom" style="text-align: center;margin-top:20px;margin-bottom: 20px;">
-                                <div>
+                                <div style="font-size:12px;">
                                     Если у вас есть вопросы, пожалуйста, напишите <br> в службу поддержки: <a href="mailto:support@course-creator.io">support@course-creator.io</a>
                                 </div>
                             </div>
-                        </body>
-                        </html>';
+                        </div>
+                    </body>
+                    </html>';
     }
 	
 	public function SendQuestion() {
