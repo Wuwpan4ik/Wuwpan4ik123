e669b3d7687359ea99b2ae6f9032156fa64dd520
Wuwpan4ik <dimalim110@gmail.com>
2023-02-02 03:35:21 +0500

    Добавить правильную проверку

--- controller/CheckFunnelSettingsController.php
+++ controller/CheckFunnelSettingsController.php
@@ -27,9 +27,15 @@ class CheckFunnelSettingsController extends FunnelController
         $videoBtnHTMLResult = str_replace('\/', '/', json_encode($popup_settings['json'], JSON_UNESCAPED_UNICODE));
         $saveVersion = $this->funnel->Get()[0]['style_settings'];
         if (!is_null($saveVersion) && ($videoBtnHTMLResult == $saveVersion)) {
-            echo 1;
-        } else {
             echo 0;
+            return false;
+        } else {
+            if (is_null($saveVersion)) {
+                echo 0;
+                return false;
+            }
+            echo 1;
+            return false;
         }
     }
 
--- template/Funnel.php
+++ template/Funnel.php
@@ -430,7 +430,7 @@
                 dataType: 'html',          /* Тип данных в ответе (xml, json, script, html). */
                 data: $("#initButton").serialize(),     /* Параметры передаваемые в запросе. */
                 success: function(data){   /* функция которая будет выполнена после успешного запроса.  */
-                    if (data == 0) {
+                    if (data == 1) {
                         document.querySelector('.exit-funnel-edit').classList.add('display-flex');
                         document.querySelector('.exit-funnel-edit').style.zIndex = '1000';
                         promise();
