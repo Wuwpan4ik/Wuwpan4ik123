bde85c1175340ddc9fc90ec18a0afd228f4cc68c
Wuwpan4ik <dimalim110@gmail.com>
2023-02-15 07:08:41 +0500

    ad

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -165,7 +165,7 @@
                 $f = $this->user->GetApi();
                 $this->api_key = $f['api_key'];
                 $this->api_endpoint = $f['endpoint'];
-                $this->EmailQueueApiCall($data);
+                $this->EmailQueueApiCall($this->api_endpoint, $this->api_key, $data);
 //                $this->SendEmail($title, $body, $email);
             }
 
--- controller/traits/SendEmail.php
+++ controller/traits/SendEmail.php
@@ -1,22 +1,73 @@
 <?php
     trait SendEmail {
+        public $result =
+        [
+        "from" => "info@litmind.com",
+        "to" => "lorenzo@tin.cat",
+        "sender" => "lorenzo@litmind.com",
+        "subject" => "Just testing",
+        "content" => "This is just an email to test Emailqueue"
+        ];
 
-        function EmailQueueApiCall($messages = false) {
-            $_SESSION['error'] = $this->api_key;
-
+        function EmailQueueApiCall($endpoint, $key, $messages = false) {
             $curl = curl_init();
 
             $request = [
-                "key" => $this->api_key,
+                "key" => $key,
                 "messages" => $messages
             ];
 
-            curl_setopt($curl, CURLOPT_URL, $this->api_endpoint);
+            curl_setopt($curl, CURLOPT_URL, $endpoint);
             curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_POSTFIELDS, ["q" => json_encode($request)]);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
             $result = curl_exec($curl);
             curl_close($curl);
             return json_decode($result, true);
+        }
+        public function SendEmail ($title, $body, $email, $file = null, $file_name = null) {
+
+            $mail = new PHPMailer\PHPMailer\PHPMailer(true);
+
+            try {
+                $mail->isSMTP();
+                $mail->CharSet = "UTF-8";
+                $mail->SMTPAuth   = true;
+                $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+                // Настройки вашей почты
+                $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+                $mail->Username   = $this->ourEmail; // Логин на почте
+                $mail->Password   = $this->ourPassword; // Пароль на почте
+                $mail->SMTPSecure = 'ssl';
+                $mail->Port       = 465;
+                $mail->smtpConnect(
+                    array(
+                        "ssl" => array(
+                            "verify_peer" => false,
+                            "verify_peer_name" => false,
+                            "allow_self_signed" => true
+                        )
+                    )
+                );
+                $mail->setFrom($this->ourEmail, $this->ourNickName); // Адрес самой почты и имя отправителя
+
+                // Получатель письма
+                $mail->addAddress($email);
+
+                $mail->isHTML();
+                $mail->Subject = $title;
+                $mail->Body = $body;
+                if (!is_null($file)) {
+                    $mail->addAttachment($file, $file_name);
+                }
+
+                if ($mail->send()) {$result = "success";}
+                else {$result = "allGood";}
+
+            } catch (Exception $e) {
+                $result = $mail->ErrorInfo;
+                $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+            }
         }
     }
