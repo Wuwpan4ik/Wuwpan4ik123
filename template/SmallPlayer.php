26a256106521496b6850aaf15ec5bcf811253413
Wuwpan4ik <dimalim110@gmail.com>
2023-03-05 00:36:19 +0500

    Добавил аватарку

--- template/SmallPlayer.php
+++ template/SmallPlayer.php
@@ -46,7 +46,7 @@
                 <div class="slider__header _conatiner-player ">
                     <div class="slider__header-logo">
                         <div class="slider__header-logo-img">
-                            <img width="48px" src="/<? echo (isset($item['avatar'])  ? $item['avatar'] : "uploads/ava/userAvatar.jpg") ?>" alt="">
+                            <img width="48px" src="/<? echo (!empty($item['avatar'])  ? $item['avatar'] : "uploads/ava/userAvatar.jpg") ?>" alt="">
                         </div>
                         <div class="slider__header-logo-text">
                             <?=$item['first_name']?>
