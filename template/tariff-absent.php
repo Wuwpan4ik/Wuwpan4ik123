66016fd923b292ec13fcf6bce692b7dad9afba5c
Wuwpan4ik <dimalim110@gmail.com>
2023-03-10 16:52:59 +0500

    Соединил ветки

--- template/tariff-absent.php
+++ template/tariff-absent.php
@@ -13,19 +13,5 @@
 </head>
 
 <body>
-<div class="userLogin">
-    <div class="userLogin-body _container">
-        <div class="user__logo">
-            <div class="user__logo-img"><img src="../img/Logo.svg" alt=""></div>
-            <div class="user__logo-text">Course Creator</div>
-        </div>
-        <div class="userLogin-popup popup" style="font-size: 18px; font-weight: 600;line-height: 24px;">
-            К большому сожалению, пользователь не оплатил тариф.
-        </div>
-        <div class="user-text">
-            <a href="/">Вернуться на сайт</a>
-        </div>
-    </div>
-</div>
 </body>
 </html>
